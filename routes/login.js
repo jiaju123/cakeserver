14714cf798ef8a907ed73d8804f6c77042bc2d33
zhangyu97 <396471308@qq.com>
2018-05-23 21:21:36 +0800

    cakeserver

--- routes/login.js
+++ routes/login.js
@@ -3,12 +3,21 @@
  */
 var express = require('express');
 var router = express.Router();
+var svgCaptcha = require('svg-captcha');
 var query = require("../lib/pool");
 
+router.get('/code', function (req, res) {
+    var captcha = svgCaptcha.createMathExpr({
+        color: true,
+        // background: '#fff'
+    });
+    req.session.code = captcha.text;
+    res.type('svg'); // 使用ejs等模板时如果报错 res.type('html')
+    res.status(200).send(captcha.data);
+});
+
 router.post('/check',function (req,res) {
-    console.log(req.body);
     let zhanghao=req.body.zhanghao;
-    console.log(zhanghao);
     let pass=req.body.pass;
     // let code=req.body.code
     // if(code.toUpperCase()!==req.session.code.toUpperCase()){
--- routes/user.js
+++ routes/user.js
@@ -5,13 +5,33 @@ var express = require('express');
 var router = new express();
 var query = require("../lib/pool");
 
-router.get('/',function(req,res){
-    query("select * from user",function(err,data){
-        if (err) throw err;
+router.get('/', function(req, res) {
+    query("select * from user",function (err, data) {
+        if (err) {
+            throw err;
+            return;
+        }
         res.json(data);
-    })
-})
+    });
+});
 
+router.post('/upd', function(req, res) {
+    console.log(req.body);
+    // let id=req.body.id;
 
+    let pass=req.body.pass;
+    console.log(pass)
+    query(`update user set pass='${pass}' where zhanghao='admin'`,function (err, data) {
+        if (err) {
+            throw err;
+            return;
+        }
+        if(data.affectedRows===1){
+            res.send("1")
+        }else {
+            res.send("0");
+        }
+    });
+});
 
 module.exports = router;
