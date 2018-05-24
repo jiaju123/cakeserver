6baf6e8ca1b711f86cf4d3bdc5d34627d2c6ac7e
MalikCheng <296062888@qq.com>
2018-05-24 14:52:53 +0800

    das

--- routes/index/incar.js
+++ routes/index/incar.js
@@ -1,3 +1,10 @@
 /**
  * Created by Administrator on 2018/5/24.
  */
+var express = require('express');
+var router = new express();
+var query = require("../lib/pool");
+
+
+
+module.exports = router;
