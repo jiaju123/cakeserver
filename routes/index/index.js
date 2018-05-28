3140448bc14750848aab79fc88dd9c63558a672b
shwleague <3073835130@qq.com>
2018-05-28 15:46:26 +0800

    'aa'

--- routes/index/index.js
+++ routes/index/index.js
@@ -5,6 +5,10 @@ var express = require('express');
 var router = new express();
 var query = require("../../lib/pool");
 
-
+router.get('/',function(req,res){
+    query(' select * from goods',function(err,data){
+        res.json(data);
+    })
+});
 
 module.exports = router;
--- routes/index/innav.js
+++ routes/index/innav.js
@@ -5,6 +5,32 @@ var express = require('express');
 var router = new express();
 var query = require("../../lib/pool");
 
+router.get('/',function(req,res){
+    query("select * from category",function(err,data){
+        if (err) throw err;
+        res.json(data);
+    })
+});
 
+router.post('/regist',function(req,res){
+    let zhanghao=req.body.zhanghao;
+    let pass=req.body.pass;
+
+    query("select * from user where zhanghao="+zhanghao,function(err,data){
+        if (err) throw err;
+       if(data){
+           res.send('123')
+       }else{
+           query(`insert into user(zhanghao,pass) value('${zhanghao}','${pass}')`,function(err,data){
+               if(data.affectedRows===1){
+                   res.send('ok');
+               }else{
+                   throw err;
+               }
+           })
+       }
+    })
+
+});
 
 module.exports = router;
