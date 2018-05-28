585996fd55eacf803b60605d43d18102e560bdf8
zhangyu97 <396471308@qq.com>
2018-05-28 09:23:43 +0800

    zy

--- routes/index/incate.js
+++ routes/index/incate.js
@@ -5,6 +5,8 @@ var express = require('express');
 var router = new express();
 var query = require("../../lib/pool");
 
-
+// router.get('/cate',function (req,res) {
+//     res.send(body);
+// })
 
 module.exports = router;
