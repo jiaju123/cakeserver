e821cfe89044a786b4254c0423dfa276b38a0054
zhaoxin111111 <554777530@qq.com>
2018-05-28 15:14:31 +0800

    4444

---
+++ routes/index/indexcar.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2018/5/24.
+ */
+var express = require('express');
+var router = new express();
+var query = require("../../lib/pool");
+
+router.get('/',function(req,res){
+    query(' select * from orders',function(err,data){
+        res.json(data);
+    })
+});
+
+
+
+module.exports = router;
