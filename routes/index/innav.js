8969c9a6b9962200ff0ba2802a6c3338ee613bfb
shwleague <3073835130@qq.com>
2018-05-31 00:36:43 +0800

    'as'

--- routes/index/innav.js
+++ routes/index/innav.js
@@ -16,21 +16,50 @@ router.post('/regist',function(req,res){
     let zhanghao=req.body.zhanghao;
     let pass=req.body.pass;
 
-    query("select * from user where zhanghao="+zhanghao,function(err,data){
-        if (err) throw err;
-       if(data){
-           res.send('123')
-       }else{
-           query(`insert into user(zhanghao,pass) value('${zhanghao}','${pass}')`,function(err,data){
-               if(data.affectedRows===1){
-                   res.send('ok');
-               }else{
-                   throw err;
-               }
-           })
-       }
+
+    query(`select * from user where zhanghao='${zhanghao}'`,function (err,result) {
+        if(err){
+            throw err
+        }
+        if(result.length == 0){
+            query(`insert into user(zhanghao,pass) value('${zhanghao}','${pass}')`,function(err,data){
+                if(data.affectedRows===1){
+                    res.send('ok');
+                }else{
+                    throw err;
+                }
+            })
+        }else{
+            res.send('账号存在')
+        }
+    })
+
+
+
+});
+
+router.post('/login',function (req,res) {
+    let zhanghao=req.body.zhanghao;
+    let pass=req.body.pass;
+
+    query(`select * from user where zhanghao='${zhanghao}'`,function (err,result) {
+        if(err){
+            throw err
+        }
+        if(result.length!==0){
+            if(pass===result[0].pass){
+                req.session.login='yes';
+                req.session.zhanghao=zhanghao;
+                res.send('ok')
+            }else{
+                res.send('密码错误')
+            }
+        }else{
+            res.send('账号不存在')
+        }
     })
 
 });
+
 
 module.exports = router;
