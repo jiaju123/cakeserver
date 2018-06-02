c9922d1cf14dc60c9103c1a7a451c3d587fa2ad7
MalikCheng <296062888@qq.com>
2018-06-02 14:55:40 +0800

    dasd

--- routes/index/inabout.js
+++ routes/index/inabout.js
@@ -5,6 +5,31 @@ var express = require('express');
 var router = new express();
 var query = require("../../lib/pool");
 
+router.post('/',function (req,res) {
+    let uid = req.body.id;
+    query("select * from address where uid ="+ uid,function (err,body) {
+        res.send(body);
+    })
+});
+
+router.post('/address',function (req,res) {
+    let city=req.body.city;
+    let name=req.body.name;
+    let address=req.body.address;
+    let tel=req.body.tel;
+    let zipcode=req.body.zipcode;
+    let uid=req.body.uid;
+
+
+    query(`insert into address (city,address,zipcode,name,tel,uid) values ('${city}','${address}','${zipcode}','${name}','${tel}','${uid}')`,function(err,data){
+        if(data.affectedRows===1){
+            let id=data.insertId;
+            query("select * from address where id ="+ id,function (err,body) {
+                res.send(body);
+            })
+        }
+    });
+});
 
 
 module.exports = router;
