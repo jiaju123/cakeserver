9404dc10a72768f31b00c69e417749049fd942d3
zhangyu97 <396471308@qq.com>
2018-06-02 14:05:18 +0800

    zy

--- routes/index/incar.js
+++ routes/index/incar.js
@@ -6,11 +6,28 @@ var router = new express();
 var query = require("../../lib/pool");
 
 router.get('/car',function (req,res) {
-    let uid = req.query.id;
-    query("select * from car where gid ="+ gid,function (err,body) {
+
+    query("select * from car",function (err,body) {
         res.send(body);
-        console.log(body);
     })
-})
+});
+router.get('/cate',function (req,res) {
+    let id = req.query.gid;
+    query(`select * from goods where id='${id}'`,function (err,body) {
+        if(err) throw err;
+        if(sql.affectedRows==1){
+            res.send('ok')
+        }else{
+            res.send('aaa')
+        }
+    })
+});
+router.get("/delcar", function(req, res) {
+    let id = req.query.id;
+    query(`delete from car where id='${id}'`), function (err, sql) {
+        // if (err) throw err;
+        res.send(sql);
+    }
+});
 
 module.exports = router;
--- routes/index/incate.js
+++ routes/index/incate.js
@@ -6,79 +6,21 @@ var router = new express();
 var query = require("../../lib/pool");
 
 router.get('/',function(req,res){
-    let id=this.$route.query.id;
-    query(`select * from goods where cid='${id}'`,function(err,data){
+    query("select * from goods",function(err,data){
         res.json(data);
     })
 });
 router.post('/addcar',function(req,res){
-    let data=req.body;
-    let id=data.gid;
-    if(!req.session.login) {
-        res.send('no')
-    }else{
-        query(`insert into car (name,taste,price,count,gid) values ('','','','','${id}')`,function(err,data){
-            if (data.affectedRows===1){
-                res.send('ok');
-            }
-        });
-    }
+    let id=req.body.gid;
+    // let taste = req.body.taste;
+    let count = req.body.count;
+    query(`insert into car (count,gid) values ('${count}','${id}')`,function(err,data){
+        if(err) throw err;
+        if(data.affectedRows==1){
+            res.send('ok')
+        }else{
+            res.send('aaa')
+        }
+    });
 });
-// router.post("/addcar", function (req, res) {
-//     let name = req.body.name;
-//     let taste = req.body.taste;
-//     let price = req.body.price;
-//     let count = req.body.count;
-//     let img = req.body.img;
-//     let gid = req.body.gid;
-//     query(`insert into car (count,img,uid,price) values ('${name}','${count}'','${img}','${uid}','${price}')`,function (err,sql) {
-//         if(err) throw err;
-//         if(sql.affectedRows==1){
-//             res.send('ok')
-//         }else{
-//             res.send('aaa')
-//             console.log(aaa);
-//         }
-//     } )
-//
-// })
-// router.get('/hot', function (req, res) {
-//     let nub=req.query.nub
-//     let size=req.query.size
-//     let n=(nub-1)*size
-//     query('select * from goods', function (err, sql) {
-//         if (err) throw err;
-//         let ridarr = [];
-//         sql.forEach(val => {
-//             let rid = JSON.parse(val.rid);
-//             let flag = rid.includes(3);
-//             if (flag) {
-//                 ridarr.push(JSON.stringify(val))
-//             }
-//         });
-//         let arr=ridarr.slice(n,nub*size)
-//         // console.log(ridarr.length);
-//         res.send(arr)
-//     })
-//
-//
-// });
-// router.post("/addbuycar", function (req, res) {
-//     let name = req.body.name;
-//     let count = req.body.count;
-//     let img = req.body.img;
-//     let gid = req.body.gid;
-//     let price = req.body.price;
-//     query(`insert into car (count,pid,img,gid,price) values ('${name}','${count}','${pid}','${img}','${uid}','${price}')`,function (err,sql) {
-//         if(err) throw err;
-//         if(sql.affectedRows==1){
-//             res.send('ok')
-//         }else{
-//             res.send('aaa')
-//         }
-//     } )
-//
-// })
-
-
 module.exports = router;
